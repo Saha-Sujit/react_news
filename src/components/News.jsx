cfa6882fb2f832cc2b546ac60c1ee78f630a6a71
Sujit Saha <sahasujit14@gmail.com>
2022-06-12 12:27:36 +0530

    resolving warnings

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -21,45 +21,45 @@ const News = (props) => {
 
   return (
     <>
-      <section class="text-gray-600 body-font">
-        <div class="container px-5 py-24 mx-auto">
-          <div class="flex flex-wrap -m-4">
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap -m-4">
             {articles.map((article) => {
               return (
-                <div class="p-4 md:w-1/3">
-                  <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                <div className="p-4 md:w-1/3">
+                  <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
                     {article.urlToImage === null ? (
                       <img
-                        class="lg:h-48 md:h-36 w-full object-cover object-center"
+                        className="lg:h-48 md:h-36 w-full object-cover object-center"
                         src="https://images.unsplash.com/photo-1634693798046-b00e706c3076?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80"
                         alt="blog"
                       />
                     ) : (
                       <img
-                        class="lg:h-48 md:h-36 w-full object-cover object-center"
+                        className="lg:h-48 md:h-36 w-full object-cover object-center"
                         src={article.urlToImage}
                         alt="blog"
                       />
                     )}
 
-                    <div class="p-6">
-                      <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+                    <div className="p-6">
+                      <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
                         {props.catname ? props.catname.toUpperCase() : "ALL"}
                       </h2>
-                      <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
+                      <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
                         {article.title}
                       </h1>
-                      <p class="leading-relaxed mb-3 truncate">
+                      <p className="leading-relaxed mb-3 truncate">
                         {article.description}
                       </p>
-                      <div class="flex items-center flex-wrap ">
+                      <div className="flex items-center flex-wrap ">
                         <a
                           href={article.url}
-                          class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0"
+                          className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0"
                         >
                           Learn More
                           <svg
-                            class="w-4 h-4 ml-2"
+                            className="w-4 h-4 ml-2"
                             viewBox="0 0 24 24"
                             stroke="currentColor"
                             stroke-width="2"
